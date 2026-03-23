56c62b215589c2fec0d59aa26c6012dd7853e3e7
julianemafissoni <julianemafisoni@hotmail.com>
2026-03-23 12:32:33 -0300

    creating register product on views.py

--- estoque/views.py
+++ estoque/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
+from .forms import ProdutoForm
 
 def lista_produtos(request):
     produtos = Produto.objects.all()
     return render(request, 'estoque/lista_produtos.html', {'produtos': produtos})
+
+def criar_produtos(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm()
+    
+    return render(request, 'estoque/criar_produto.html', {'form': form})
