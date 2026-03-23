aa126bd0b197d2468a91778a13db90f662512224
julianemafissoni <julianemafisoni@hotmail.com>
2026-03-23 13:39:50 -0300

    improving the error signs when creating products

--- estoque/views.py
+++ estoque/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Produto
 from .forms import ProdutoForm
+from django.contrib import messages
 
 def lista_produtos(request):
     produtos = Produto.objects.all()
@@ -11,6 +12,7 @@ def criar_produto(request):
         form = ProdutoForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Product criado com sucesso!')
             return redirect('lista_produtos')
     else:
         form = ProdutoForm()
